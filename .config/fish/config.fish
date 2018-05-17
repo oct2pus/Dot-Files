50c7a62d2d46080cb38a5117b9d014390320449d
Austin LaBerta <oct2pus@debian.debian>
2018-05-17 13:45:13 -0500

    updated dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,11 @@
 set fish_greeting ""
+
+set PATH $HOME/.npm-packages/bin $PATH
+
+status --is-interactive; and source (rbenv init - | sed 's/setenv/set -gx/' | psub)
+
+#if which ruby >/dev/null; and which gem >/dev/null then
+ #   set PATH "(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+#end
+
+set PATH $HOME/.gem/ruby/*/gems $PATH
